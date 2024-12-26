1d49ad37687612a6a3d9af88f7e70f093aaa3cd3
Denis Pi. <zdeniz@mail.ru>
2024-12-26 11:21:07 +0300

    2

--- obj/Debug/net8.0/docker-tg-bot.AssemblyInfo.cs
+++ obj/Debug/net8.0/docker-tg-bot.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("docker-tg-bot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa87f523f7bcd2ef378561b372be066e9f710389")]
 [assembly: System.Reflection.AssemblyProductAttribute("docker-tg-bot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("docker-tg-bot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
